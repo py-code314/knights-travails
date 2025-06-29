8cd581e5b200296cf37abd1415441eaf5e2187e0
srikk <122169588+py-code314@users.noreply.github.com>
2025-06-28 20:41:09 -0400

    refactor: generateAdjacencyList()

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// const util = require('util')
 
 function knightMoves(startNode, endNode) {
   // Get adjacency list
@@ -37,7 +36,7 @@ function knightMoves(startNode, endNode) {
     }
   }
 
-  console.log('Path:', path.reverse())
+  return path.reverse()
 }
 
 function updateNodesInfo(queue, nextMoves, nodesInfo) {
@@ -94,17 +93,6 @@ function generateAdjacencyList(startNode) {
   return adjacencyList
 }
 
-// Array of move functions
-const moveFunctions = [
-  moveUpOneLeftTwoOrLeftTwoUpOne,
-  moveUpOneRightTwoOrRightTwoUpOne,
-  moveUpTwoLeftOneOrLeftOneUpTwo,
-  moveUpTwoRightOneOrRightOneUpTwo,
-  moveRightOneDownTwoOrDownTwoRightOne,
-  moveRightTwoDownOneOrDownOneRightTwo,
-  moveDownOneLeftTwoOrLeftTwoDownOne,
-  moveDownTwoLeftOneOrLeftOneDownTwo,
-]
 
 // Generate neighbors bases on the current node
 function generateNeighbors(currentNode) {
@@ -113,10 +101,28 @@ function generateNeighbors(currentNode) {
 
   const neighbors = []
 
-  moveFunctions.forEach((func) => {
-    const neighbor = func(first, second)
+  const addValues = [
+    [1, -2],
+    [1, 2],
+    [2, -1],
+    [2, 1],
+    [-2, 1],
+    [-1, 2],
+    [-1, -2],
+    [-2, -1],
+  ]
 
-    if (neighbor) {
+  addValues.forEach((values) => {
+    const nextFirst = first + values[0]
+    const nextSecond = second + values[1]
+
+    if (
+      nextFirst >= 0 &&
+      nextFirst <= 7 &&
+      nextSecond >= 0 &&
+      nextSecond <= 7
+    ) {
+      const neighbor = [nextFirst, nextSecond]
       neighbors.push(neighbor)
     }
   })
@@ -124,93 +130,9 @@ function generateNeighbors(currentNode) {
   return neighbors
 }
 
-// Move knight one step up, two steps left or two steps left, one step up
-function moveUpOneLeftTwoOrLeftTwoUpOne(first, second) {
-  const nextFirst = first + 1
-  const nextSecond = second - 2
 
-  if (nextFirst >= 0 && nextFirst <= 7 && nextSecond >= 0 && nextSecond <= 7) {
-    const neighbor = [nextFirst, nextSecond]
-    return neighbor
-  }
-}
-
-// Move knight one step up, two steps right or two steps right, one step up
-function moveUpOneRightTwoOrRightTwoUpOne(first, second) {
-  const nextFirst = first + 1
-  const nextSecond = second + 2
-
-  if (nextFirst >= 0 && nextFirst <= 7 && nextSecond >= 0 && nextSecond <= 7) {
-    const neighbor = [nextFirst, nextSecond]
-    return neighbor
-  }
-}
-
-// Move knight two steps up, one step left or one step left, two steps up
-function moveUpTwoLeftOneOrLeftOneUpTwo(first, second) {
-  const nextFirst = first + 2
-  const nextSecond = second - 1
-
-  if (nextFirst >= 0 && nextFirst <= 7 && nextSecond >= 0 && nextSecond <= 7) {
-    const neighbor = [nextFirst, nextSecond]
-    return neighbor
-  }
-}
-
-// Move knight two steps up, one step right or one step right, two steps up
-function moveUpTwoRightOneOrRightOneUpTwo(first, second) {
-  const nextFirst = first + 2
-  const nextSecond = second + 1
-
-  if (nextFirst >= 0 && nextFirst <= 7 && nextSecond >= 0 && nextSecond <= 7) {
-    const neighbor = [nextFirst, nextSecond]
-    return neighbor
-  }
-}
-
-// Move knight one step right, two steps down or two steps down, one step right
-function moveRightOneDownTwoOrDownTwoRightOne(first, second) {
-  const nextFirst = first - 2
-  const nextSecond = second + 1
-
-  if (nextFirst >= 0 && nextFirst <= 7 && nextSecond >= 0 && nextSecond <= 7) {
-    const neighbor = [nextFirst, nextSecond]
-    return neighbor
-  }
-}
-
-// Move knight two steps right, one step down or one step down, two steps right
-function moveRightTwoDownOneOrDownOneRightTwo(first, second) {
-  const nextFirst = first - 1
-  const nextSecond = second + 2
-
-  if (nextFirst >= 0 && nextFirst <= 7 && nextSecond >= 0 && nextSecond <= 7) {
-    const neighbor = [nextFirst, nextSecond]
-    return neighbor
-  }
-}
-
-// Move knight one step down, two steps left or two steps left, one step down
-function moveDownOneLeftTwoOrLeftTwoDownOne(first, second) {
-  const nextFirst = first - 1
-  const nextSecond = second - 2
-
-  if (nextFirst >= 0 && nextFirst <= 7 && nextSecond >= 0 && nextSecond <= 7) {
-    const neighbor = [nextFirst, nextSecond]
-    return neighbor
-  }
-}
-
-// Move knight two steps down, one step left or one step left, two steps down
-function moveDownTwoLeftOneOrLeftOneDownTwo(first, second) {
-  const nextFirst = first - 2
-  const nextSecond = second - 1
-
-  if (nextFirst >= 0 && nextFirst <= 7 && nextSecond >= 0 && nextSecond <= 7) {
-    const neighbor = [nextFirst, nextSecond]
-    return neighbor
-  }
-}
-
-// generateAdjacencyList([3, 3])
-knightMoves([0, 0], [4, 4])
+// knightMoves([0, 0], [4, 4])
+console.log(knightMoves([0, 0], [3, 3]))
+console.log(knightMoves([3, 3], [0, 0]))
+console.log(knightMoves([0, 0], [7, 7]))
+// knightMoves([0, 0], [4, 4])
